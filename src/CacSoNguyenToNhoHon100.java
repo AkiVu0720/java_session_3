d20104fbdf60edc1bf57fb97dcd6d21edf76bcc7
AkiVu0720 <akivu1993@gmail.com>
2023-07-22 02:59:15 +0900

    upload bai 3

--- src/CacSoNguyenToNhoHon100.java
+++ src/CacSoNguyenToNhoHon100.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class CacSoNguyenToNhoHon100 {
+import java.util.Scanner;
+
+public class CacSoNguyenToNhoHon100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Các sô nguyên tố dưới 100");
+        for (int i = 0; i <= 100; i++) {
+            if (isPreim(i)){
+                System.out.printf("%d ",i);
+            }
+        }
+
+    }
+
+    public  static  boolean isPreim(int numberCheck){
+        if (numberCheck < 2){
+            return  false;
+        }
+        for (int i = 2; i <= Math.sqrt(numberCheck); i++) {
+            if (numberCheck % i == 0){
+                return false;
+            }
+        }
+        return  true;
+
+    }
 }
